8421f8d129dbd8d127e219996bdc76e7ab95884d
mbissonho <mateus_bissonho@live.com>
2018-06-25 10:40:35 -0300

    Início do Projeto.

---
+++ app/src/main/java/br/edu/iff/pooa20181/safepass/MainActivity.java
@@ -0,0 +1,13 @@
+package br.edu.iff.pooa20181.safepass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
